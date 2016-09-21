c7019c8920c24c2f344f828970f72007aa3533f9
rkosineski <robert.j.kosineski@gmail.com>
2016-09-21 16:32:11 -0400

    renaming hello-world boilerplate to barhood-front-end

--- Public/barhood-front-end/src/App.js
+++ Public/barhood-front-end/src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-var App = React.createClass({
+class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to the thunderdome?</h2>
+          <h2>Welcome to Barhood</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -16,6 +16,6 @@ var App = React.createClass({
       </div>
     );
   }
-});
+}
 
 export default App;
